98b80ff7d2e3bcb82d02fe1ac192b8354b608792
mdkamranalam <work.mdkamranalam@gmail.com>
2026-06-13 18:03:49 +0530

    feat: wrap App component in a div for improved layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,18 @@ import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <>
+    <div className="relative overflow-hidden">
+      <div className="fixed inset-0 -z-10 bg-[#050816]" />
+      <div className="fixed top-20 left-20 w-72 h-72 bg-cyan-500/20 blur-[150px] -z-10" />
+      <div className="fixed bottom-20 right-20 w-72 h-72 bg-purple-500/20 blur-[150px] -z-10" />
+
       <Navbar />
       <Hero />
       <Features />
       <Stats />
       <CTA />
       <Footer />
-    </>
+    </div>
   );
 }
 
