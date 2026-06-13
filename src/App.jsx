3cc8acd5b7676058ebb73dbe07fc8ac2a4ed89f9
mdkamranalam <work.mdkamranalam@gmail.com>
2026-06-13 23:31:05 +0530

    feat: add ParticleBackground component for animated background effects

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Features from "./components/Features.jsx";
 import Stats from "./components/Stats.jsx";
 import CTA from "./components/CTA.jsx";
 import Footer from "./components/Footer.jsx";
+import ParticleBackground from "./components/ParticleBackground.jsx";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div className="fixed inset-0 -z-10 bg-[#050816]" />
       <div className="fixed top-20 left-20 w-72 h-72 bg-cyan-500/20 blur-[150px] -z-10" />
       <div className="fixed bottom-20 right-20 w-72 h-72 bg-purple-500/20 blur-[150px] -z-10" />
-
+      <ParticleBackground />
       <Navbar />
       <Hero />
       <Features />
